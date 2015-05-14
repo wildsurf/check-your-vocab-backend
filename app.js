c25c12de757dfb243503222a7c159921827155a8
Veronika Hillebrand <wildsurf.now@gmail.com>
2015-05-14 18:47:46 +0200

    Adding local, twitter and fb login

--- app.js
+++ app.js
@@ -10,15 +10,19 @@ var routes = require('./routes/index');
 var quiz = require('./routes/quiz');
 var words = require('./routes/words');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+var configDB = require('./config/database');
 
-mongoose.connect('mongodb://localhost/check_your_vocab');
+mongoose.connect(configDB.url);
 
 var app = express();
 
 // view engine setup
 app.set('port', (process.env.PORT || 3000));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 var cors = require('cors');
 var corsOptions = {
@@ -35,9 +39,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// passport
+require('./config/passport')(passport);
+app.use(session({ secret: 'alrededor' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// routes
 app.use('/', routes);
 app.use('/quiz', quiz);
 app.use('/words', words);
+
+require('./routes/users')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/database.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  url: 'mongodb://localhost/check_your_vocab'
+};
